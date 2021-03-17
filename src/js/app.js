cba221679d50cf04fa860f0916951d86b0d29a51
Joseph Erxleben <jerxleben.13@gmail.com>
2021-03-17 00:22:17 -0400

    Added basic functionality, butbakery.js is not linking for some reason.

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+
+const ovenButton = document.querySelector('.input__oven');
+const donutCounterElement = document.querySelector('.hud__donutCounter');
+
+const updateDonutCounter = function(donutCounterElement, bakery){
+    donutCounterElement.innerText = bakery.getDonut();
+} 
+
+const makeButtonIntoOven = (oven, donutCounter, bakery) =>{
+    oven.addEventListener('click', ()=>{
+        bakery.bake();
+        updateDonutCounter(donutCounter, bakery);
+    })
+}
+
+const appBakery = new bakery();
+
+makeButtonIntoOven(ovenButton, donutCounterElement, appBakery);
--- src/js/bakery.js
+++ src/js/bakery.js
@@ -4,4 +4,12 @@ class Bakery {
         this._autoclicker = 0;
         this._multiplier = 0;
     }
+
+    bake () {
+        this._donuts += 1;
+    }
+
+    getDonuts() {
+        return this._donuts;
+    }
 }
