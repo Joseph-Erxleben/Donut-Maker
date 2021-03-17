9d8e118d454a742fe55d0519004ba101508ca86a
Joseph Erxleben <jerxleben.13@gmail.com>
2021-03-17 01:04:07 -0400

    Price of auto clickers now increases exponentially.

--- src/js/bakery.js
+++ src/js/bakery.js
@@ -2,6 +2,7 @@ class Bakery {
     constructor () {
         this._donuts = 0;
         this._autoClicker = 0;
+        this._autoClickerCost = 100;
         this._multiplier = 0;
     }
 
@@ -15,7 +16,8 @@ class Bakery {
 
     addAutoClicker() {
         this._autoClicker += 1;
-        this._donuts -= 100;
+        this._donuts -= this._autoClickerCost;
+        this._autoClickerCost *= 1.1;
     }
 
     getAutoClicker() {
