7ae417b45e237901338f6c16a78d9621f016d686
Joseph Erxleben <jerxleben.13@gmail.com>
2021-03-19 11:29:15 -0400

    Moved math out of ticking function.

--- src/js/bakery.js
+++ src/js/bakery.js
@@ -28,6 +28,7 @@ class Bakery {
             this._autoClicker += 1;
             this._donuts -= this._autoClickerCost;
             this._autoClickerCost *= 1.1;
+            this._donutsPerSecond = this._autoClicker * this._donutsPerClick;
         }
         else{
 
@@ -38,9 +39,9 @@ class Bakery {
     addMultiplier(){
         if(this._donuts >= this._multiplierCost){
             this._multiplier += 1;
-            this._donutsPerClick = this._multiplierValue ** this._multiplier;
             this._donuts -= this._multiplierCost;
             this._multiplierCost *= 1.1;
+            this._donutsPerClick = this._multiplierValue ** this._multiplier;
         }
         else{
 
@@ -63,8 +64,7 @@ class Bakery {
         return this._multiplierCost;
     }
 
-    activateAutoClickers = () =>{
-        this._donutsPerSecond = this._autoClicker * this._donutsPerClick;
+    activateAutoClickers = () =>{  
         this._donuts += this._donutsPerSecond;
     }
 }
